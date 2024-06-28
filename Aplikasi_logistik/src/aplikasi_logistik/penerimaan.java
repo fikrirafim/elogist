445401844e4e5290b49d93c6cdfd5b72d9d62fdb
endline117 <endline117@gmail.com>
2024-06-28 19:21:26 +0700

    perbaikan fungsi get id dari textfield menjadi combobox pada penerimaan

--- Aplikasi_logistik/src/aplikasi_logistik/penerimaan.java
+++ Aplikasi_logistik/src/aplikasi_logistik/penerimaan.java
@@ -35,16 +35,15 @@ public class penerimaan extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         TFnopenerimaan = new javax.swing.JTextField();
-        TFidproduk = new javax.swing.JTextField();
         TFqty = new javax.swing.JTextField();
         TFtanggal = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        Bsimpan = new javax.swing.JButton();
+        Bclear = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
+        CBproduk = new javax.swing.JComboBox<>();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -62,18 +61,14 @@ public class penerimaan extends javax.swing.JFrame {
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setText("No. Penerimaan");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 80, 99, -1));
-
-        jLabel2.setText("ID Produk");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, 99, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, 99, -1));
 
         jLabel3.setText("QTY");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, 37, -1));
 
         jLabel4.setText("Tanggal Penerimaan");
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, 120, -1));
-        jPanel1.add(TFnopenerimaan, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 80, 200, -1));
-        jPanel1.add(TFidproduk, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 200, -1));
+        jPanel1.add(TFnopenerimaan, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 200, -1));
 
         TFqty.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -83,24 +78,36 @@ public class penerimaan extends javax.swing.JFrame {
         jPanel1.add(TFqty, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 160, 200, -1));
         jPanel1.add(TFtanggal, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 200, 200, -1));
 
-        jButton1.setText("Simpan");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Bsimpan.setText("Simpan");
+        Bsimpan.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                BsimpanActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 250, -1, -1));
+        jPanel1.add(Bsimpan, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 250, -1, -1));
 
-        jButton2.setText("Clear");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Bclear.setText("Clear");
+        Bclear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                BclearActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 250, -1, -1));
+        jPanel1.add(Bclear, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 250, -1, -1));
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplikasi_logistik/icon/ic penerimaan.png"))); // NOI18N
         jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 60, -1, -1));
+
+        CBproduk.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Produk" }));
+        CBproduk.addAncestorListener(new javax.swing.event.AncestorListener() {
+            public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
+                CBprodukAncestorAdded(evt);
+            }
+            public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
+            }
+            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
+            }
+        });
+        jPanel1.add(CBproduk, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 70, -1, -1));
 
         jMenu1.setText("E-logist");
         jMenuBar1.add(jMenu1);
@@ -173,22 +180,25 @@ public class penerimaan extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void BclearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BclearActionPerformed
         TFnopenerimaan.setText("");
-        TFidproduk.setText("");
+//        TFidproduk.setText("");
         TFqty.setText("");
         TFtanggal.setText("");
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_BclearActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void BsimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BsimpanActionPerformed
         try{
-            sql = "INSERT INTO strukpenerimaan_t (nostrukpenerimaan, produkfk, qty, tanggal_penerimaan) VALUE (?,?,?,?)";
+            sql = "INSERT INTO stokproduk_t (norec, produkfk, qty) VALUE ("
+                    +TFnopenerimaan.getText()+","
+                    +CBproduk.getSelectedItem()+","
+                    +TFqty.getText()+")";
             PreparedStatement pst = con.prepareStatement(sql);
 
-            pst.setString(1, TFnopenerimaan.getText());
-            pst.setString(2, TFidproduk.getText());
-            pst.setString(3, TFqty.getText());
-            pst.setString(4, TFtanggal.getText());
+//            pst.setString(1, TFnopenerimaan.getText());
+//            pst.setString(2, CBproduk.getItem());
+//            pst.setString(3, TFqty.getText());
+//            pst.setString(4, TFtanggal.getText());
             int affectedRow = pst.executeUpdate();
 
             if(affectedRow > 0 ){
@@ -199,7 +209,7 @@ public class penerimaan extends javax.swing.JFrame {
         }catch(Exception e){
             JOptionPane.showMessageDialog(this, e.getMessage());
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_BsimpanActionPerformed
 
     private void TFqtyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TFqtyActionPerformed
 
@@ -225,6 +235,20 @@ public class penerimaan extends javax.swing.JFrame {
         new pengiriman().setVisible(true);
         dispose();
     }//GEN-LAST:event_jRadioButtonMenuItem4ActionPerformed
+
+    private void CBprodukAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_CBprodukAncestorAdded
+        try{
+            sql = "SELECT id FROM produk_m";
+            stat = con.createStatement();
+            rs = stat.executeQuery(sql);
+            
+            while(rs.next()){
+                CBproduk.addItem(rs.getString("id"));
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(this, e.getMessage());
+        }
+    }//GEN-LAST:event_CBprodukAncestorAdded
 
     /**
      * @param args the command line arguments
@@ -262,14 +286,13 @@ public class penerimaan extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField TFidproduk;
+    private javax.swing.JButton Bclear;
+    private javax.swing.JButton Bsimpan;
+    private javax.swing.JComboBox<String> CBproduk;
     private javax.swing.JTextField TFnopenerimaan;
     private javax.swing.JTextField TFqty;
     private javax.swing.JTextField TFtanggal;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
